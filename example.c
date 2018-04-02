2e08a5b12baff553f43a4cba6e84d707e733101c
SomeAnotherDude <need.an.email@yandex.ru>
2018-04-02 06:30:03 +0300

    v1.1

--- example.c
+++ example.c
@@ -70,12 +70,50 @@ printf("%d\n", out); // prints "1"
 
 asm("template-asm" : OUTPUT : INPUT : CLOBBERS);
 
-// OUTPUT is a comma-separated 
+// OUTPUT and INPUT is a comma-separated lists of "constraints"(var)
+//   Constraints says whether an operand may be in a register and which kind of register 
+//   or it's a memory reference and which kinds of address, etc...
+//   i.e "a" means "value must be stored in eax", 
+//   "r" means "put value in any register you want" and so on.
+//   "=" means "this variable is a write-only variable", "+" means read-write
+
+//   here is most used constraints
+
++---+--------------------+
+| r |    Register(s)     |
++---+--------------------+
+| a |   %eax, %ax, %al   |
+| b |   %ebx, %bx, %bl   |
+| c |   %ecx, %cx, %cl   |
+| d |   %edx, %dx, %dl   |
+| S |   %esi, %si        |
+| D |   %edi, %di        |
++---+--------------------+
+
+// there are many others, look at http://ibiblio.org/gferg/ldp/GCC-Inline-Assembly-HOWTO.html
 
 
+// CLOBBERS are comma-separated list of register which you code affects (all the registers you write in your code)
+//   it tells to compiler that it shouldn't count on them anymore, it must preserve they values if it needs to
+
+int a = 1;
+int c;
+
+asm("mov edx %1;\
+	 add edx, %2;\
+	 mov edx %0"
+	: "=r"(c) // "c" variable can be placed in any register and will be rewritten by the asm 
+	: "r"(a), "r"(2) // input may be an expression 
+	: "edx" ); // this code rewrites "edx" register so we mention it in CLOBBERS list 
 
 
+// %0 .. %2 means "values in order of apperance from output to input, from left to right"
+// e.g %0 = a, %1 = b, %2 = c, %3 = d
+asm("..." : "=r"(a), "=r"(b) : "r"(c), "r"(d))
 
-
-
+// a variable might be input and output same time
+asm("add %0, %2" 
+	: "=r"(a) // %0
+	: "0"(a),  // explicitly set %0 to a but %1 = a anyway
+	  "r"(b) );	
 
